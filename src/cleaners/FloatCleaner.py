5fcc9bd7266672160d5bc93ddb859c564b8931c4
chetansinghsajwan <chetanss@jmangroup.com>
2025-05-30 14:04:42 +0000

    update: float cleaner add money range

--- src/cleaners/FloatCleaner.py
+++ src/cleaners/FloatCleaner.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Literal
 from .. import math_utils
 from .ColCleaner import ColCleaner
 
@@ -9,8 +10,12 @@ class FloatCleaner(ColCleaner):
     Class to clean float columns in a DataFrame.
     """
 
+    range: Literal["f128", "money"] = "f128"
+
     # Precision of the decimal
     precision: int = 2
+
+    _precision: int = field(init=False, repr=False)
 
     def __post_init__(self):
 
@@ -18,14 +23,16 @@ class FloatCleaner(ColCleaner):
 
         assert self.precision <= 38, "Precision must be less than or equal to 38"
 
-        object.__setattr__
-        self.datatype = f"decimal(38, {self.precision})"
+        if self.range == "money":
+            precision = 3
+        else:
+            precision = 2
+
+        object.__setattr__(self, "_precision", precision)
+        object.__setattr__(self, "datatype", f"decimal(38, {precision})")
 
     def clean_value(self, value: str | None):
 
-        parsed_value = math_utils.parse_float(value)
-        if parsed_value is None:
-            raise ValueError(f"Cannot parse '{value}' as decimal")
-
+        parsed_value = float(value)
         parsed_value = math_utils.floor_float(parsed_value, self.precision)
         return parsed_value
